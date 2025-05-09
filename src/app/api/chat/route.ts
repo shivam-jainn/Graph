026f45cd58579125dc8af2d7d58c1ce9cc4e2fba
shivam-jainn <shivam.clgstash@gmail.com>
2025-05-09 15:01:00 +0530

    A basic chat app is ready

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,19 @@
+import { groq } from '@ai-sdk/groq';
+import { streamText } from 'ai';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = streamText({
+    model: groq('gemma2-9b-it'),
+    messages,
+    system: `You are a helpful assistant. Check your knowledge base before answering any questions.
+    Only respond to questions using information from tool calls.
+    if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`
+  });
+
+  return result.toDataStreamResponse();
+}
