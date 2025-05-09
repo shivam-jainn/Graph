f7fff7ff552a3b31b0c88e5a32e0bd1879a2847c
shivam-jainn <shivam.clgstash@gmail.com>
2025-05-09 23:19:53 +0530

    removed prompt

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -9,10 +9,7 @@ export async function POST(req: Request) {
 
   const result = streamText({
     model: groq('gemma2-9b-it'),
-    messages,
-    system: `You are a helpful assistant. Check your knowledge base before answering any questions.
-    Only respond to questions using information from tool calls.
-    if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`
+    messages
   });
 
   console.log(result);
